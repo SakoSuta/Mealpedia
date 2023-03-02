71a3b0e2710924126ee16721e4a87830c60ce98f
Emilie <EmilieMontpre@outlook.com>
2023-03-02 18:50:08 +0100

    data recovery attempt

--- src/views/RandomView.vue
+++ src/views/RandomView.vue
@@ -6,13 +6,25 @@ export default {
       .then(response => response.json())
       .then(json => {
         console.log(json)
-        this.Random = json
+        this.RandomCategorie = json.meals[0].strCategory,
+        this.RandomName = json.meals[0].strMeal,
+        this.RandomOrigin = json.meals[0].strArea,
+        this.RandomImage = json.meals[0].strMealThumb;
+
+        for (let i = 1; i <= 20; i++) {
+      this['RandomIngre' + i] = json.meals['strIngredient' + i];
+      }
       })
       .catch(error => console.error(error));
   },
   data() {
     return {
-        Random: []
+        RandomCategorie: "",
+        RandomName: "",
+        RandomOrigin: "",
+        RandomImage: "",
+
+        RandomIngre1: ""
     }
   }
 }
@@ -21,7 +33,12 @@ export default {
 <template>
   <div class="random">
     <h1>Meal Random</h1>
-    <h1>{{ Random }}</h1>
+    <h2>Titre :</h2>
+    <h3>{{ RandomImage }}</h3>
+    <h3>{{ RandomName }}</h3>
+    <h3>{{ RandomCategorie }}</h3>
+    <h3>{{ RandomOrigin }}</h3>
+    <h3>{{ RandomIngre1 }}</h3>
   </div>
 </template>
 
@@ -30,6 +47,7 @@ export default {
   .random {
     min-height: 100vh;
     display: flex;
+    flex-direction: column;
     align-items: center;
   }
 }
