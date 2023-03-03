bcd94e0bb916ac40a8406df77d76bc369f6bd661
Emilie <EmilieMontpre@outlook.com>
2023-03-03 10:55:02 +0100

    Api random functional

--- src/views/RandomView.vue
+++ src/views/RandomView.vue
@@ -9,10 +9,11 @@ export default {
         this.RandomCategorie = json.meals[0].strCategory,
         this.RandomName = json.meals[0].strMeal,
         this.RandomOrigin = json.meals[0].strArea,
-        this.RandomImage = json.meals[0].strMealThumb;
+        this.RandomImage = json.meals[0].strMealThumb,
+        console.log(json.meals[0]['strIngredient' + 1])
 
         for (let i = 1; i <= 20; i++) {
-      this['RandomIngre' + i] = json.meals['strIngredient' + i];
+      this['RandomIngre' + i] = json.meals[0]['strIngredient' + i];
       }
       })
       .catch(error => console.error(error));
@@ -24,7 +25,26 @@ export default {
         RandomOrigin: "",
         RandomImage: "",
 
-        RandomIngre1: ""
+        RandomIngre1: "a",
+        RandomIngre2: "a",
+        RandomIngre3: "a",
+        RandomIngre4: "a",
+        RandomIngre5: "a",
+        RandomIngre6: "a",
+        RandomIngre7: "a",
+        RandomIngre8: "a",
+        RandomIngre9: "a",
+        RandomIngre10: "a",
+        RandomIngre11: "a",
+        RandomIngre12: "a",
+        RandomIngre13: "a",
+        RandomIngre14: "a",
+        RandomIngre15: "a",
+        RandomIngre16: "a",
+        RandomIngre17: "a",
+        RandomIngre18: "a",
+        RandomIngre19: "a",
+        RandomIngre20: "a",
     }
   }
 }
@@ -33,12 +53,17 @@ export default {
 <template>
   <div class="random">
     <h1>Meal Random</h1>
-    <h2>Titre :</h2>
-    <h3>{{ RandomImage }}</h3>
-    <h3>{{ RandomName }}</h3>
-    <h3>{{ RandomCategorie }}</h3>
-    <h3>{{ RandomOrigin }}</h3>
-    <h3>{{ RandomIngre1 }}</h3>
+    <img v-bind:src=RandomImage alt="Image" width=200>
+    <h2>Titre : {{ RandomName }}</h2>
+    <h3>Categorie : {{ RandomCategorie }}</h3>
+    <h3>Origin : {{ RandomOrigin }}</h3>
+    <h3>Ingredients : </h3>
+    <ul>
+  <li v-for="i in 20" :key="i" v-if="this['RandomIngre' + i] !== 'a'">{{ this['RandomIngre' + i] }}</li>
+</ul>
+
+
+
   </div>
 </template>
 
